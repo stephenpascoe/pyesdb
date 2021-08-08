7934f773d50d2f66eace6729c8d0cfc83b783f82
Stephen Pascoe <stephen.pascoe@lirico.co.uk>
2021-08-08 11:09:53 +0100

    Another test

--- tests/integration/test_streams.py
+++ tests/integration/test_streams.py
@@ -19,3 +19,17 @@ def test_write_stream(es_client):
     print(resp)
 
     assert resp.WhichOneof('result') == 'success'
+
+
+def test_write_read_stream(es_client):
+    es_client.delete_stream(b'integration-testing')
+
+    events = [SomeEvent(f'A test event with {x}', x) for x in range(20)]
+
+    resp = es_client.send_events(b'integration-testing', events)
+    assert resp.WhichOneof('result') == 'success'
+
+    events_it = es_client.iter_stream(b'integration-testing', count=100)
+    new_events = list(events_it)
+
+    assert len(new_events) == len(events)
